bfd57fbfe4e20f314fbbe9502b5e3d2545b21266
wolf-syntax <chetansharma7040@gmail.com>
2026-06-17 17:03:35 +0530

    Create Middle of the Linked List.cpp

---
+++ Week3/Day1/Middle of the Linked List.cpp
@@ -0,0 +1,15 @@
+using namespace std;
+class Solution {
+    public :
+    ListNode*middleNode(ListNode*head){
+        ListNode*slow = head;
+        ListNode*fast =head;
+        while(fast != nullptr&&fast->next !=nullptr ){
+            slow = slow->next;
+            fast = fast->next->next;
+
+        }
+        return slow;
+
+    }
+};
