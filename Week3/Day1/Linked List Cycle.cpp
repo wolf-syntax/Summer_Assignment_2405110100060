eecf4b648382db851e1f7a3c37f795f8231d3b2b
wolf-syntax <chetansharma7040@gmail.com>
2026-06-17 16:36:40 +0530

    Create Linked List Cycle.cpp

---
+++ Week3/Day1/Linked List Cycle.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+class Solution {
+    public:
+    bool hasCycle(ListNode*head){
+        if(head == nullptr||head->next == nullptr){
+            return false;
+        }
+        ListNode *slow = head;
+        ListNode *fast= head;
+        while(fast != nullptr && fast->next != nullptr){
+            slow= slow->next;
+            fast= fast->next->next;
+            if(slow == fast ){
+                return true;
+            }
+        }
+        return false;
+
+    }
+};
