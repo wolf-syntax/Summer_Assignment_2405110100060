a309e4d2da169e8ab60dd404b91c2faf0335b5bf
wolf-syntax <chetansharma7040@gmail.com>
2026-07-02 15:44:59 +0530

    Create Number of Recent Calls.cpp

---
+++ Week3/Day5/Number of Recent Calls.cpp
@@ -0,0 +1,24 @@
+#include <queue>
+using namespace std;
+
+class RecentCounter {
+    queue<int> q;
+    
+public:
+    RecentCounter() {
+    
+    }
+    
+    int ping(int t) {
+        
+        q.push(t);
+        
+        
+        while (q.front() < t - 3000) {
+            q.pop();
+        }
+        
+    
+        return q.size();
+    }
+};
