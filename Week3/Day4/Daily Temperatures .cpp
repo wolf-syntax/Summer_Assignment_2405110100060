e9e6d23b29685eacf51bccd0e5ee4531a7777f0f
wolf-syntax <chetansharma7040@gmail.com>
2026-07-02 15:30:09 +0530

    Create  Daily Temperatures .cpp

---
+++ Week3/Day4/Daily Temperatures .cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int> result(n, 0); 
+        stack<int> st; 
+        
+        for (int i = 0; i < n; i++) {
+            
+            while (!st.empty() && temperatures[i] > temperatures[st.top()]) {
+                int prevIndex = st.top();
+                st.pop();
+                result[prevIndex] = i - prevIndex;
+            }
+            st.push(i); 
+        }
+        
+        return result;
+    }
+};
