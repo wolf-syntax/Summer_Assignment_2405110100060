2665d60f584700f46140279c2c95a5903645be7f
wolf-syntax <chetansharma7040@gmail.com>
2026-07-02 15:35:30 +0530

    Create  Implement Queue using Stacks.cpp

---
+++ Week3/Day5/Implement Queue using Stacks.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <stack>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack<char> st;
+        for (char c : s) {
+           
+            if (!st.empty() && st.top() == c) {
+                st.pop();
+            } else {
+               
+                st.push(c);
+            }
+        }
+        
+        
+        string res = "";
+        while (!st.empty()) {
+            res += st.top();
+            st.pop();
+        }
+        reverse(res.begin(), res.end()); 
+        return res;
+    }
+};
